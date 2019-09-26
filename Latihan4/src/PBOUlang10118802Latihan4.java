2e2ad21ec65361a6b823f29b01ca889bc3dd3be4
iantreza19 <55836534+iantreza19@users.noreply.github.com>
2019-09-26 20:41:56 +0700

    Add files via upload

---
+++ Latihan4/src/PBOUlang10118802Latihan4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IantReza
+ */
+public class PBOUlang10118802Latihan4 { 
+//nama kelasnya tidak saya ganti kambing, soalnya buat format pengumpulantugas
+
+    public void tambahKambing(){
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: "+ jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBOUlang10118802Latihan4 kambingJantan = new PBOUlang10118802Latihan4();
+        kambingJantan.tambahKambing();
+    }
+    
+}
